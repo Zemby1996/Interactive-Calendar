3ad2156ed783720ad9060ad3d30f98eb11bdd6bc
Zemby1996 <158062891+Zemby1996@users.noreply.github.com>
2025-06-15 19:43:59 +0200

    Creating a template for backend and frontend with connection to DB in Neon

---
+++ backend/interactive-calendar-backend/src/main/java/pl/calendar/interactive_calendar_backend/InteractiveCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.calendar.interactive_calendar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveCalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveCalendarBackendApplication.class, args);
+	}
+
+}
---
+++ backend/interactive-calendar-backend/src/test/java/pl/calendar/interactive_calendar_backend/InteractiveCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.calendar.interactive_calendar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveCalendarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
