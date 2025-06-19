d7609af2575ddc0b989243eb6b4b0dda9c17bb4e
Zemby1996 <158062891+Zemby1996@users.noreply.github.com>
2025-06-19 06:56:43 +0200

    Adding fronetnd to sec.config

--- backend/interactive-calendar-backend/src/main/java/pl/calendar/interactive_calendar_backend/config/SecurityConfig.java
+++ backend/interactive-calendar-backend/src/main/java/pl/calendar/interactive_calendar_backend/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "https://interactive-calendar-frontend.netlify.app"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
